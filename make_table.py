6bbbe89c110899f99e50195febb08713287b74de
Ruoka Mestari <ruokaarpa@gmail.com>
2017-02-22 09:38:11 +0200

    migrate fix

--- make_table.py
+++ make_table.py
@@ -6,14 +6,15 @@ cur = con.cursor()
 
 cur.execute("CREATE TABLE paikat(id INTEGER PRIMARY KEY ASC, nimi TEXT)")
 
-cur.execute("CREATE TABLE matka(id INTEGER FOREIGN KEY , kaukana INTEGER)")
+cur.execute("CREATE TABLE matka(paikka INTEGER, kaukana INTEGER, FOREIGN KEY(paikka) REFERENCES paikat(id))")
 
-cur.execute("""CREATE TABLE ominaisuudet(id INTEGER FOREIGN KEY,
+cur.execute("""CREATE TABLE ominaisuudet(paikka INTEGER FOREIGN KEY,
                 tasalaatuisuus INTEGER,
                 parkkipaikka INTEGER,
                 palvelu INTEGER,
                 hinta INTEGER,
-                bonus INTEGER)""")
+                bonus INTEGER,
+                FOREIGN KEY(paikka) REFERENCES paikat(id))""")
 
 cur.commit()
 cur.close()

f988a98cb4341165fdbf63bac0e5f7828e0eba20
Ruoka Mestari <ruokaarpa@gmail.com>
2017-02-22 09:42:15 +0200

    migrate fix 2

--- make_table.py
+++ make_table.py
@@ -8,7 +8,7 @@ cur.execute("CREATE TABLE paikat(id INTEGER PRIMARY KEY ASC, nimi TEXT)")
 
 cur.execute("CREATE TABLE matka(paikka INTEGER, kaukana INTEGER, FOREIGN KEY(paikka) REFERENCES paikat(id))")
 
-cur.execute("""CREATE TABLE ominaisuudet(paikka INTEGER FOREIGN KEY,
+cur.execute("""CREATE TABLE ominaisuudet(paikka INTEGER,
                 tasalaatuisuus INTEGER,
                 parkkipaikka INTEGER,
                 palvelu INTEGER,

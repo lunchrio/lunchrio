3ea87593a80d1fd99ca6a0b549692aaf85c62fb3
Ruoka Mestari <ruokaarpa@gmail.com>
2017-02-22 09:27:58 +0200

    fixed migration

--- make_table.py
+++ make_table.py
@@ -4,16 +4,16 @@ con = sqlite3.connect("ruoka.db")
 
 cur = con.cursor()
 
-cursor.execute("CREATE TABLE paikat(id INTEGER PRIMARY KEY ASC, nimi TEXT)")
+cur.execute("CREATE TABLE paikat(id INTEGER PRIMARY KEY ASC, nimi TEXT)")
 
-cursor.execute("CREATE TABLE matka(id INTEGER FOREIGN KEY , kaukana INTEGER)")
+cur.execute("CREATE TABLE matka(id INTEGER FOREIGN KEY , kaukana INTEGER)")
 
-cursor.execute("""CREATE TABLE ominaisuudet(id INTEGER FOREIGN KEY,
+cur.execute("""CREATE TABLE ominaisuudet(id INTEGER FOREIGN KEY,
                 tasalaatuisuus INTEGER,
                 parkkipaikka INTEGER,
                 palvelu INTEGER,
                 hinta INTEGER,
                 bonus INTEGER)""")
 
-cursor.commit()
-cursor.close()
+cur.commit()
+cur.close()
